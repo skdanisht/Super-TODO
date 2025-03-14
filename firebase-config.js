69d28786dc78c160bbc8f340e72a71ee790a455f
Danish Tanvir <76249700+skdanisht@users.noreply.github.com>
2025-03-14 11:58:59 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -11,6 +11,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
 const db = firebase.firestore();
-const googleProvider = new firebase.auth.GoogleAuthProvider();
